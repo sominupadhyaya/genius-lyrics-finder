ff42d3272d9b65d9c6de2edf82bc61cb1d8468ec
Somin <sominupadhyaya@gmail.com>
2023-01-18 19:55:17 +0545

    complete the useFetchData hook in data

--- src/data/fetch.ts
+++ src/data/fetch.ts
@@ -1,5 +1,10 @@
-export const fetchData = () =>{
-    
+import { useQuery } from "@tanstack/react-query"
+
+export const useFetchData = (url:string) =>{
+    const {isLoading , error, data} = useQuery({
+        queryKey : ['lyrics-data'],
+        queryFn : () => fetch(url).then(res => console.log(res))
+    })
 }
 
 
